0477b3e82ba63c13741f887b5ac64351580e672b
tibaas <igorox2@hotmail.com>
2023-07-10 01:10:14 -0300

    Update

--- src/components/InfoScreen.jsx
+++ src/components/InfoScreen.jsx
@@ -6,8 +6,8 @@ export function InfoScreen(){
         <div className={styles.clipboardScreen}>
             <img src={Clipboard} alt="Logo-todo" />
             
-                <p1> <strong>Você ainda não tem tarefas cadastradas</strong></p1>
-                <p1> Crie tarefas e organize seus itens a fazer</p1>
+                <span> <strong>Você ainda não tem tarefas cadastradas</strong></span>
+                <span> Crie tarefas e organize seus itens a fazer</span>
             
         </div>
     )
